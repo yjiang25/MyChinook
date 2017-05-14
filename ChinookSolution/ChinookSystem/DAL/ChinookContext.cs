5aacf807d497cbaa96ee364336cbe65a50301c62
Allan Anderson <allana@shaw.ca>
2017-05-14 13:45:35 -0700

    day1

---
+++ ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+#endregion 
+namespace ChinookSystem.DAL
+{
+    //This is an internal class for security reasons
+    //Access is restricted to within this class library project
+    //Inherits DnContext for Entity FrameWork,which requires
+    //System.Data.Entity
+    internal class ChinookContext : DbContext
+    {
+        //Pass the connection string name to the 
+        //DbContext using :base()
+        public ChinookContext() : base("ChinookDB")
+        { }
+    }
+}
